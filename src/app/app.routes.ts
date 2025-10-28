143267f699772640dffecd5c90ac8c9a2aee1ebb
Jhenriquens <joaoferri2004@gmail.com>
2025-10-27 21:17:33 -0300

    rotas arrumadas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './features/auth/login/login.component';
+import { VendasComponent } from './features/vendas/vendas.component';
+import { HistoricoComponent } from './features/historico/historico.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'login', component: LoginComponent },
+  { path: 'vendas', component: VendasComponent },
+  { path: 'historico', component: HistoricoComponent },
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  { path: '**', redirectTo: 'login' }
+];
